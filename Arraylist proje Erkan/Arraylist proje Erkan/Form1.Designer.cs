c6b8ab2e73977faecfa7d77077c9d6f412944863
erkanozer <100263348+123456erkan@users.noreply.github.com>
2022-03-07 12:08:21 +0300

    Arraylist proje 6

---
+++ Arraylist proje Erkan/Arraylist proje Erkan/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Arraylist_proje_Erkan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGüncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.txtOkulNo = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.listeOgrenciler = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(110, 136);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(84, 26);
+            this.btnAra.TabIndex = 0;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(209, 136);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(84, 26);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGüncelle
+            // 
+            this.btnGüncelle.Location = new System.Drawing.Point(110, 200);
+            this.btnGüncelle.Name = "btnGüncelle";
+            this.btnGüncelle.Size = new System.Drawing.Size(84, 26);
+            this.btnGüncelle.TabIndex = 2;
+            this.btnGüncelle.Text = "Güncelle";
+            this.btnGüncelle.UseVisualStyleBackColor = true;
+            this.btnGüncelle.Click += new System.EventHandler(this.btnGüncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(209, 200);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(84, 26);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtOkulNo
+            // 
+            this.txtOkulNo.Location = new System.Drawing.Point(76, 32);
+            this.txtOkulNo.Name = "txtOkulNo";
+            this.txtOkulNo.Size = new System.Drawing.Size(100, 20);
+            this.txtOkulNo.TabIndex = 4;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(76, 68);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(172, 20);
+            this.txtAdSoyad.TabIndex = 5;
+            // 
+            // listeOgrenciler
+            // 
+            this.listeOgrenciler.FormattingEnabled = true;
+            this.listeOgrenciler.Location = new System.Drawing.Point(309, 54);
+            this.listeOgrenciler.Name = "listeOgrenciler";
+            this.listeOgrenciler.Size = new System.Drawing.Size(120, 290);
+            this.listeOgrenciler.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Okul No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 399);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listeOgrenciler);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.txtOkulNo);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGüncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.btnAra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGüncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.TextBox txtOkulNo;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.ListBox listeOgrenciler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Arraylist proje Erkan/Arraylist proje Erkan/boll.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arraylist_proje_Erkan
+{
+    class boll
+    {
+    }
+}
